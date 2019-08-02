4ce6f1395713f851be76443638eb11b4d681a179
Greg Jones <gtjones1@aggies.ncat.edu>
2019-08-01 21:22:00 -0500

    snipcart added

--- .history/src/components/Home/Product_20190801210444.js
+++ .history/src/components/Home/Product_20190801210444.js
@@ -14,8 +14,7 @@ export default function Product({product}) {
             data-item-id={product.id}
             data-item-name={product.title}
             data-item-price={product.price}
-            data-item-image={product.image.fluid.src}
-            data-item-url="https://island-jerk.netlify.com/"
+            data-item-url="http://myapp.com/products/bacon"
             >
                 Add to cart
             </button>
